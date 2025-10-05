98d41d34945f66ce4be5893b70cc600771e4fc21
peeyush2025 <sudhakarln82@gmail.com>
2025-10-05 19:26:01 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,14 @@
+import random
+playing=True
+number=str(random.randint(10,20))
+print("I will generate a number from 0 to 9, and you have to guess the number one digit at a time.")
+print("the game ends when you get 1 hero!")
+while playing:
+    guess= input ("give me your best guess! \n")
+    if number ==guess:
+        print("you won the game")
+        print("the number was",number)
+        break
+    else:
+        print("your guess isn't quite right,try again.\n")
+        
